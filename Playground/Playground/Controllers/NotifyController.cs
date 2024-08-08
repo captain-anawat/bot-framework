9a178d0808104ef1e0df62b4175199da8eab6fe9
Anawat <anawat@perfenterprise.com>
2024-08-08 10:36:13 +0700

    added notify api about cancel and done

--- Playground/Playground/Controllers/NotifyController.cs
+++ Playground/Playground/Controllers/NotifyController.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace Playground.Controllers
 {
-    [Route("api/notify")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class NotifyController : ControllerBase
     {
@@ -29,43 +29,90 @@ namespace Playground.Controllers
             _conversationReferences = conversationReferences;
             _appId = configuration["MicrosoftAppId"] ?? string.Empty;
         }
-        
-        public async Task<IActionResult> Get()
+
+        [HttpGet()]
+        public async Task<IActionResult> Request()
         {
             foreach (var conversationReference in _conversationReferences.Values)
             {
-                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference, BotCallback, default(CancellationToken));
+                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference, RequestBotCallback, default(CancellationToken));
             }
             return Ok();
-        }
 
-        private async Task BotCallback(ITurnContext turnContext, CancellationToken cancellationToken)
-        {
-            var APIBaseUrl = "https://delivery-3rd-test-api.azurewebsites.net";
-            var RiderId = "637937263065127099";
-            var riderDetailsApi = $"{APIBaseUrl}/api/Rider/GetRiderInfo/{RiderId}";
-            var request = await _restClientService.Get<EmployeeDetails>(riderDetailsApi);
-
-            if (request.OrderRequest == null)
+            async Task RequestBotCallback(ITurnContext turnContext, CancellationToken cancellationToken)
             {
-                var message = "ระบบส่ง noti แต่ไม่มีออเดอร์";
-                var text = MessageFactory.Text(message);
-                await turnContext.SendActivityAsync(text);
-                return;
-            }
+                var APIBaseUrl = "https://delivery-3rd-test-api.azurewebsites.net";
+                var RiderId = "637937263065127099";
+                var riderDetailsApi = $"{APIBaseUrl}/api/Rider/GetRiderInfo/{RiderId}";
+                var request = await _restClientService.Get<EmployeeDetails>(riderDetailsApi);
 
-            var card = new HeroCard
-            {
-                Title = $"ออเดอร์ {request.OrderRequest.OrderCode}",
-                Subtitle = $"ร้าน {request.OrderRequest.Restaurant.Name}{Environment.NewLine}{request.OrderRequest.Restaurant.Address}{Environment.NewLine}ผู้รับ {request.OrderRequest.Customer.Name}{Environment.NewLine}{request.OrderRequest.Customer.Address}{Environment.NewLine}{request.OrderRequest.Customer.Remark}",
-                Text = "รับงานภายใน 30 วินาที",
-                Buttons = new List<CardAction> {
+                if (request.OrderRequest == null)
+                    return;
+
+                var card = new HeroCard
+                {
+                    Title = $"ออเดอร์ {request.OrderRequest.OrderCode}",
+                    Subtitle = $"ร้าน {request.OrderRequest.Restaurant.Name}{Environment.NewLine}{request.OrderRequest.Restaurant.Address}{Environment.NewLine}ผู้รับ {request.OrderRequest.Customer.Name}{Environment.NewLine}{request.OrderRequest.Customer.Address}{Environment.NewLine}{request.OrderRequest.Customer.Remark}",
+                    Text = "รับงานภายใน 30 วินาที",
+                    Buttons = new List<CardAction> {
                     new(ActionTypes.ImBack, title: "รับออเดอร์", value: $"รับออเดอร์ {request.OrderRequest._id}")
                 }
-            };
-            var attachment = card.ToAttachment();
-            var reply = MessageFactory.Attachment(attachment);
-            await turnContext.SendActivityAsync(reply, cancellationToken);
+                };
+                var attachment = card.ToAttachment();
+                var reply = MessageFactory.Attachment(attachment);
+                await turnContext.SendActivityAsync(reply, cancellationToken);
+            }
+        }
+
+        [HttpGet("{riderId}/{orderId}")]
+        public async Task<IActionResult> Cancel(string riderId, string orderId)
+        {
+            foreach (var conversationReference in _conversationReferences.Values)
+            {
+                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference, CancelBotCallback, default(CancellationToken));
+            }
+            return Ok();
+
+            async Task CancelBotCallback(ITurnContext turnContext, CancellationToken cancellationToken)
+            {
+                var messageText = "คำขอยกเลิกออเดอร์ได้รับการอนุมัติ";
+                var reply = MessageFactory.Text(messageText, messageText);
+                await turnContext.SendActivityAsync(reply, cancellationToken);
+            }
+        }
+
+        [HttpGet("{riderId}/{orderId}")]
+        public async Task<IActionResult> CancelDeny(string riderId, string orderId)
+        {
+            foreach (var conversationReference in _conversationReferences.Values)
+            {
+                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference, CancelDenyBotCallback, default(CancellationToken));
+            }
+            return Ok();
+
+            async Task CancelDenyBotCallback(ITurnContext turnContext, CancellationToken cancellationToken)
+            {
+                var messageText = "คำขอยกเลิกออเดอร์ถูกปฎิเสธ";
+                var reply = MessageFactory.Text(messageText, messageText);
+                await turnContext.SendActivityAsync(reply, cancellationToken);
+            }
+        }
+
+        [HttpGet("{riderId}/{orderId}")]
+        public async Task<IActionResult> Done(string riderId, string orderId)
+        {
+            foreach (var conversationReference in _conversationReferences.Values)
+            {
+                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference, DoneBotCallback, default(CancellationToken));
+            }
+            return Ok();
+
+            async Task DoneBotCallback(ITurnContext turnContext, CancellationToken cancellationToken)
+            {
+                var messageText = "คุณส่งออเดอร์เรียบร้อย";
+                var reply = MessageFactory.Text(messageText, messageText);
+                await turnContext.SendActivityAsync(reply, cancellationToken);
+            }
         }
     }
 
